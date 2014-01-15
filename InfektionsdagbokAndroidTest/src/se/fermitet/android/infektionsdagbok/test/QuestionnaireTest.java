a1bd5e049ebe75605467ab37fe1d62ff3bfd9228
Fredrik Ferm <fredrik@fermitet.se>
2014-01-16 00:23:46 +0100

    Incomplete - task : Basic questions ToggleButtons

--- InfektionsdagbokAndroidTest/src/se/fermitet/android/infektionsdagbok/test/QuestionnaireTest.java
+++ InfektionsdagbokAndroidTest/src/se/fermitet/android/infektionsdagbok/test/QuestionnaireTest.java
@@ -4,6 +4,8 @@ import com.robotium.solo.Solo;
 
 import se.fermitet.android.infektionsdagbok.Quesionnaire;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class QuestionnaireTest extends ActivityInstrumentationTestCase2<Quesionnaire> {
 
@@ -36,9 +38,33 @@ public class QuestionnaireTest extends ActivityInstrumentationTestCase2<Quesionn
 		checkForText("Slemhosta");
 		checkForText("Morgonupphostning");
 		checkForText("VŠsentligen frisk");
+		
+		checkToggleButton("malaise");
+		checkToggleButton("fever");
+		checkToggleButton("earAche");
+		checkToggleButton("soreThroat");
+		checkToggleButton("runnyNose");
+		checkToggleButton("stommacAche");
+		checkToggleButton("dryCough");
+		checkToggleButton("wetCough");
+		checkToggleButton("morningCough");
+		checkToggleButton("generallyWell");
 	}
 	
 	private void checkForText(String text)  {
 		assertTrue("Should have text: " + text, solo.searchText(text));
 	}
+	
+	private void checkToggleButton(String name) {
+		View view = solo.getView("@id/" + name + "SCT");
+		assertNotNull(view);
+		
+		assertTrue(name + " view should be a ToggleButton", view instanceof ToggleButton);
+		ToggleButton tb = (ToggleButton) view;
+		
+		assertEquals(name + " toggle button off text", "Nej", tb.getTextOff().toString());
+		assertEquals(name + " toggle button on text", "Ja", tb.getTextOn().toString());
+	}
+
 }
+
