f00a23a38249355fe904b68ac3ff06788d6063ba
Fredrik Ferm <fredrik@fermitet.se>
2014-02-06 00:25:02 +0100

    Incomplete - task : Save Spike for saving instead of storing in memory (commented out in ModelManager)

--- Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/model/ModelManager.java
+++ Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/model/ModelManager.java
@@ -11,11 +11,13 @@ public class ModelManager {
 
 	private Map<Week, WeekAnswers> weekAnswers;
 	private Context context;
+	/* private Storage storage; */
 
 	public ModelManager(Context context) {
 		super();
 		this.context = context;
 		this.weekAnswers = new HashMap<Week, WeekAnswers>();
+		/* this.storage = new Storage(context);*/
 	}
 
 	public WeekAnswers getInitialWeekAnswers() {
@@ -24,18 +26,45 @@ public class ModelManager {
 	}
 
 	public WeekAnswers getPreviousWeekAnswers(WeekAnswers initial) {
+		/* try {
+			 			storage.saveAnswers(initial);
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }*/
+
 		return getWeekAnswersForWeek(initial.week.previous());
 	}
 
 	public WeekAnswers getNextWeekAnswers(WeekAnswers initial) {
+		/* try {
+			 			storage.saveAnswers(initial);
+		 } catch (Exception e) {
+			 e.printStackTrace();
+		 }*/
+
 		return getWeekAnswersForWeek(initial.week.next());
 	}
 
 	public void reset() {
 		weekAnswers.clear();
+		/* storage.clear();*/
 	}
 
 	private WeekAnswers getWeekAnswersForWeek(Week weekToUse) {
+		/*WeekAnswers ret = null;
+		try {
+			ret = storage.getAnswersForWeek(weekToUse);
+
+			if (ret == null) {
+				ret = new WeekAnswers(weekToUse);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return ret;*/
+
 		WeekAnswers ret = weekAnswers.get(weekToUse);
 
 		if (ret == null) {
