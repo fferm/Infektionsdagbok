45226dca12646cf8e6dd2537210ba4737d7c5858
Fredrik Ferm <fredrik@fermitet.se>
2014-03-07 00:18:54 +0100

    Incomplete - task : Get notification up Add notification button

---
+++ Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/activity/NotificationStarter.java
@@ -0,0 +1,15 @@
+package se.fermitet.android.infektionsdagbok.activity;
+
+import se.fermitet.android.infektionsdagbok.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationStarter extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.notification_starter);
+	}
+}
