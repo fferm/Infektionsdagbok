d0035c693909e78e89692f116c5d26bc14c3e740
Fredrik Ferm <fredrik@fermitet.se>
2014-03-10 23:58:38 +0100

    Notification is showing

--- Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/activity/NotificationStarter.java
+++ Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/activity/NotificationStarter.java
@@ -1,8 +1,16 @@
 package se.fermitet.android.infektionsdagbok.activity;
 
+import se.fermitet.android.infektionsdagbok.Questionnaire;
 import se.fermitet.android.infektionsdagbok.R;
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
 
 public class NotificationStarter extends Activity {
 
@@ -11,5 +19,25 @@ public class NotificationStarter extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.notification_starter);
+	}
+	
+	public void buttonPressed(View v) {
+		NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+		builder.setSmallIcon(R.drawable.ic_action_next_item); // TODO: Better notification icon
+		builder.setContentTitle("Infektionsdagbok");
+		builder.setContentText("Fyll i veckans infektionsdagbok");
+		
+		Intent resultIntent = new Intent(this, Questionnaire.class);
+		
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		stackBuilder.addParentStack(Questionnaire.class);
+		stackBuilder.addNextIntent(resultIntent);
+		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+		builder.setContentIntent(resultPendingIntent);
+		
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		int myId = 231231;
+		notificationManager.notify(myId, builder.build());
 	}
 }
