1cdf3a171dedbfa8447ba1e308b31918cf07e4eb
Fredrik Ferm <fredrik@fermitet.se>
2014-10-07 11:25:48 +0200

    Refactor: ExportView används nu

--- Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/activity/ExportActivity.java
+++ Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/activity/ExportActivity.java
@@ -1,15 +1,20 @@
 package se.fermitet.android.infektionsdagbok.activity;
 
 import se.fermitet.android.infektionsdagbok.R;
-import android.app.Activity;
+import se.fermitet.android.infektionsdagbok.views.ExportView;
 import android.os.Bundle;
+import android.view.View;
 
-public class ExportActivity extends Activity {
+public class ExportActivity extends InfektionsdagbokActivity {
+
+	private ExportView view = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.export_view);
+        view = (ExportView) View.inflate(this, R.layout.export_view, null);
+        setContentView(view);
+
 	}
 }
