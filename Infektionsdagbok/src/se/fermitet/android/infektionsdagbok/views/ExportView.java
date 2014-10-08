1fe4f372ee6a631911abf3f6224089edff699c1e
Fredrik Ferm <fredrik@fermitet.se>
2014-10-08 23:39:59 +0200

    Starting spike of excel file

--- Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/views/ExportView.java
+++ Infektionsdagbok/src/se/fermitet/android/infektionsdagbok/views/ExportView.java
@@ -1,7 +1,18 @@
 package se.fermitet.android.infektionsdagbok.views;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DateFormat;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.joda.time.DateTime;
 
 import se.fermitet.android.infektionsdagbok.R;
@@ -118,7 +129,77 @@ public class ExportView extends RelativeLayout implements View.OnClickListener {
 			String msg = "Kan inte ha startdatum efter slutdatum";
 			Toast toast = Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT);
 			toast.show();
+			return;
 		}
+		
+		System.out.println("!!!! file write success is " + saveExcelFile());
 	}
 
+	private boolean saveExcelFile() { 
+		Context context = getContext();
+		String fileName = "Infektionsdagbok.xls";
+		
+        // check if available and not read only 
+ /*       if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) { 
+            Log.e(TAG, "Storage not available or read only"); 
+            return false; 
+        }*/ 
+ 
+        boolean success = false; 
+ 
+        //New Workbook
+        Workbook wb = new HSSFWorkbook();
+ 
+        Cell c = null;
+ 
+        //Cell style for header row
+        CellStyle cs = wb.createCellStyle();
+        cs.setFillForegroundColor(HSSFColor.LIME.index);
+        cs.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        
+        //New Sheet
+        Sheet sheet1 = null;
+        sheet1 = wb.createSheet("myOrder");
+ 
+        // Generate column headings
+        Row row = sheet1.createRow(0);
+ 
+        c = row.createCell(0);
+        c.setCellValue("Item Number");
+        c.setCellStyle(cs);
+ 
+        c = row.createCell(1);
+        c.setCellValue("Quantity");
+        c.setCellStyle(cs);
+ 
+        c = row.createCell(2);
+        c.setCellValue("Price");
+        c.setCellStyle(cs);
+ 
+        sheet1.setColumnWidth(0, (15 * 500));
+        sheet1.setColumnWidth(1, (15 * 500));
+        sheet1.setColumnWidth(2, (15 * 500));
+ 
+        // Create a path where we will place our List of objects on external storage 
+        File file = new File(context.getExternalFilesDir(null), fileName); 
+        FileOutputStream os = null; 
+ 
+        try { 
+            os = new FileOutputStream(file);
+            wb.write(os);
+//            Log.w("FileUtils", "Writing file" + file); 
+            success = true; 
+        } catch (IOException e) { 
+  //          Log.w("FileUtils", "Error writing " + file, e); 
+        } catch (Exception e) { 
+    //        Log.w("FileUtils", "Failed to save file", e); 
+        } finally { 
+            try { 
+                if (null != os) 
+                    os.close(); 
+            } catch (Exception ex) { 
+            } 
+        } 
+        return success; 
+    } 
 }
