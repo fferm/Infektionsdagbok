8ac211c3a1028a57cc9862e29da4c84e88349b1c
Fredrik Ferm <fredrik@fermitet.se>
2014-10-29 09:43:06 +0100

    Timeout on adapter test for adding treatments

--- InfektionsdagbokAndroidTest/src/se/fermitet/android/infektionsdagbok/activity/TreatmentActivityTest_NoTestData.java
+++ InfektionsdagbokAndroidTest/src/se/fermitet/android/infektionsdagbok/activity/TreatmentActivityTest_NoTestData.java
@@ -19,7 +19,7 @@ public class TreatmentActivityTest_NoTestData extends ActivityTestWithSolo<Treat
 	public TreatmentActivityTest_NoTestData() {
 		super(TreatmentActivity.class);
 	}
-	
+
 	public void testEnterNewTreatmentFromEmpty() throws Exception {
 		DateTime startDate = DateTime.now().withMillisOfDay(0);
 		int numDays = 100;
@@ -41,33 +41,42 @@ public class TreatmentActivityTest_NoTestData extends ActivityTestWithSolo<Treat
 		solo.enterText((EditText) solo.getView(R.id.numDaysEdit), "" + numDays);
 		solo.enterText((EditText) solo.getView(R.id.medicineEdit), medicine);
 		solo.enterText((EditText) solo.getView(R.id.infectionTypeEdit), infectionType);
-		
+
 		// Click save
 		solo.clickOnView(solo.getView(R.id.saveBTN));
-		
+
 		// Check saved data
 		Map<UUID, Treatment> allFromFile = null;
 		setStart();
 		do {
 			allFromFile = getActivity().getLocalApplication().getModelManager().getAllTreatments();
-			
+
 			setElapsed();
 		} while (allFromFile.size() != 1 && notYetTimeout());
 		System.out.println("!!! elapsed: " + elapsed);
 		assertEquals("Size after save", 1, allFromFile.size());
-		
+
 		Treatment fromFile = allFromFile.values().iterator().next();
-		
+
 		assertEquals("Start", startDate, fromFile.getStartingDate());
 		assertEquals("Num days", numDays, fromFile.getNumDays());
 		assertEquals("Medicine", medicine, fromFile.getMedicine());
 		assertEquals("InfectionType", infectionType, fromFile.getInfectionType());
-		
+
 		// Check in adapter
 		ListAdapter adapter = getListAdapter();
-		assertEquals("adapter size",  1, adapter.getCount());
+
+		int count;
+		setStart();
+		do {
+			count = adapter.getCount();
+
+			setElapsed();
+		} while (count != 1 && notYetTimeout());
+		assertEquals("adapter size",  1, count);
+
 		Treatment inAdapter = (Treatment) adapter.getItem(0);
-		
+
 		assertEquals("Start", startDate, inAdapter.getStartingDate());
 		assertEquals("Num days", numDays, inAdapter.getNumDays());
 		assertEquals("Medicine", medicine, inAdapter.getMedicine());
